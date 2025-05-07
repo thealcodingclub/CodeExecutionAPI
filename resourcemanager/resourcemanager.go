161ef91b948b94f7e7f8e5e6e2befb1017f81776
Anga205 <github@angadbhalla.com>
2025-05-07 22:16:42 +0530

    added mutexes to avoid memory spillage

--- resourcemanager/resourcemanager.go
+++ resourcemanager/resourcemanager.go
@@ -1,8 +1,16 @@
 package resourcemanager
 
-var availableMemory int = 6000000 // 6GB in KB
+import "sync"
+
+var (
+	availableMemory int = 6000000 // 6GB in KB
+	memoryMutex     sync.Mutex
+)
 
 func ReserveMemory(memory int) bool {
+	memoryMutex.Lock()
+	defer memoryMutex.Unlock()
+
 	if availableMemory >= memory {
 		availableMemory -= memory
 		return true
@@ -11,5 +19,8 @@ func ReserveMemory(memory int) bool {
 }
 
 func ReleaseMemory(memory int) {
+	memoryMutex.Lock()
+	defer memoryMutex.Unlock()
+
 	availableMemory += memory
 }
